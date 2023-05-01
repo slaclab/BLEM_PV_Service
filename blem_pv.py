0e0eb7f2190992aae4d1f5336606715f6a743bc0
Zach Domke <zdomke@slac.stanford.edu>
2023-05-01 10:50:53 -0700

    Add comments & change TWISS_KEYS to list

--- blem_pv.py
+++ blem_pv.py
@@ -7,6 +7,7 @@ from p4p.client.thread import Context
 from matlab.engine import (start_matlab, InterruptedError)
 
 
+# Establish logging
 logger = getLogger()
 handler = StreamHandler()
 formatter = Formatter('[%(asctime)s] [%(levelname)-8s] - %(message)s')
@@ -16,20 +17,25 @@ logger.setLevel("WARNING")
 handler.setLevel("WARNING")
 
 
-TWISS_KEYS = {'p0c': 0,
-              'psi_x': 1,
-              'beta_x': 2,
-              'alpha_x': 3,
-              'eta_x': 4,
-              'etap_x': 5,
-              'psi_y': 6,
-              'beta_y': 7,
-              'alpha_y': 8,
-              'eta_y': 9,
-              'etap_y': 10}
+# Ordered list of dict keys used by TWISS NTTable
+TWISS_KEYS = ['p0c', 'psi_x', 'beta_x', 'alpha_x', 'eta_x', 'etap_x',
+              'psi_y', 'beta_y', 'alpha_y', 'eta_y', 'etap_y']
 
 
 def update_pv_value(pv, n, devices, z_pos, l_eff, r_mat=None, twiss=None):
+    """Save data to the NTTable, starting with the common data for RMAT
+    and TWISS PVs, then doing one or the other. Requires user to provide
+    either r_mat or twiss data, but not both.
+
+    Args:
+        pv (NTTable): The data recieved from PVA and will be written to.
+        n (list): The element names from MATLAB's model_rMatGet().
+        devices (list): The device names for each element.
+        z_pos (2-D list): The z-positions of elements.
+        l_eff (2-D list): The effective lengths of elements.
+        r_mat (3-D list): The transport matrices for elements.
+        twiss (2-D list): The twiss parameters for elements.
+    """
     if r_mat is None and twiss is None:
         raise ValueError("Provide one of the following r_mat or twiss")
     elif r_mat is not None and twiss is not None:
@@ -48,16 +54,28 @@ def update_pv_value(pv, n, devices, z_pos, l_eff, r_mat=None, twiss=None):
                 pv['value'][key] = r_mat[i][j]
 
     if twiss is not None:
-        for key, ind in TWISS_KEYS.items():
+        for ind, key in enumerate(TWISS_KEYS):
             pv['value'][key] = twiss[ind]
 
 
 def periodic_pvs(pva, m_eng, element_devices_dict, b_path, p_type):
+    """Update the RMAT & TWISS PVs for the given Beam Path and Model
+    Type. This is done by running the MATLAB function model_rMatGet()
+    and saving the data in associated PVs.
+
+    Args:
+        pva (Context): The PVAccess Context used to get/write PVA PVs.
+        m_eng (MatlabEngine): The MATLAB Engine used to get data.
+        element_devices_dict (dict): Dictionary containing element and
+            device names.
+        b_path (str): The Beam Path of the requested data.
+        p_type (str): The Model Type of the requested data.
+    """
     logger.info(f"{b_path}:{p_type}\tStart")
 
     get_type = "EXTANT" if p_type == "LIVE" else "DESIGN"
 
-    # Run the Matlab function model_rMatGet() (the energy variable is unused)
+    # Run the MATLAB function model_rMatGet() (the energy variable is unused)
     r_mat, z_pos, l_eff, twiss, _, n = m_eng.model_rMatGet(
         b_path,
         [],
@@ -67,6 +85,7 @@ def periodic_pvs(pva, m_eng, element_devices_dict, b_path, p_type):
 
     devices = [element_devices_dict.get(ele, ele) for ele in n]
 
+    # Save RMAT data; TypeError thrown if data includes complex numbers
     try:
         r_mat_pv = pva.get(f"BLEM:SYS0:1:{b_path}:{p_type}:RMAT")
         update_pv_value(r_mat_pv, n, devices, z_pos, l_eff, r_mat=r_mat)
@@ -77,6 +96,7 @@ def periodic_pvs(pva, m_eng, element_devices_dict, b_path, p_type):
     except TypeError as e:
         logger.error(f"{b_path}:{p_type}:RMAT\t{e.args[0]}")
 
+    # Save TWISS data; TypeError thrown if data includes complex numbers
     try:
         twiss_pv = pva.get(f"BLEM:SYS0:1:{b_path}:{p_type}:TWISS")
         update_pv_value(twiss_pv, n, devices, z_pos, l_eff, twiss=twiss)
@@ -91,12 +111,17 @@ def periodic_pvs(pva, m_eng, element_devices_dict, b_path, p_type):
 
 
 def get_element_dict(m_eng):
+    """Create a dictionary where key = an element name used by the model
+    data, and value = a device name to be saved to the PV.
+    """
+    # Get list of all element names (ignore non-str & duplicates)
     element_set = set()
     for e in m_eng.model_nameConvert('*', 'MAD'):
         if type(e) is str:
             element_set.add(e)
     elements = list(element_set)
 
+    # Get a device name for each element name (non-str -> '')
     devices = []
     for d in m_eng.model_nameConvert(elements):
         if type(d) is str:
@@ -108,16 +133,25 @@ def get_element_dict(m_eng):
 
 
 def main():
+    """For the given Beam Path and Model Type, open a MATLAB engine and
+    repeatedly (<= 1Hz) populate the associated PV with model data.
+    """
+    # Parse arguments passed into the program
     parser = ArgumentParser(prog="BLEM PV Service")
     parser.add_argument("b_path", metavar="Beam Path")
     parser.add_argument("p_type", choices=['LIVE', 'DESIGN'])
     args = parser.parse_args()
 
+    # Open a PVAccess connection with p4p
     pva = Context('pva', nt=False)
 
+    # Start a MATLAB engine
     m_eng = start_matlab()
+
+    # Populate a dictionary of {element name (str): device name (str)}
     element_devices_dict = get_element_dict(m_eng)
 
+    # Populate associated PV continuously; at most once per second
     try:
         while True:
             start_time = time()
