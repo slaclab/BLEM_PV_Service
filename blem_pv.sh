55383581a9682dd90116fde8723a5b7394a9043e
Zach Domke <zdomke@slac.stanford.edu>
2023-08-02 16:21:05 -0700

    Prep bash script for Cron Job

--- blem_pv.sh
+++ blem_pv.sh
@@ -1,16 +1,34 @@
 #!/bin/bash
 
-cd "$(dirname "${BASH_SOURCE[0]}")"
-
-# Source Python3.7 to use Matlab 2020a w/ python's matlab.engine
+# Source Default Environment & Python3.7 to use Matlab 2020a w/ python's matlab.engine
+source /afs/slac/g/lcls/tools/script/ENVS64.bash
 source $PACKAGE_TOP/anaconda/envs/python3.7env/bin/activate
 export PYTHONPATH=$TOOLS/python/toolbox
 export LD_LIBRARY_PATH=/usr/local/lcls/package/anaconda/envs/python3.7env/epics/lib/linux-x86_64:$LD_LIBRARY_PATH
 
+# Change Directory to the directory that contains the necessary python file
+cd "$(dirname "${BASH_SOURCE[0]}")"
 
 # Run each script in the background and store their process IDs
 b_paths=("CU_HXR" "CU_SXR" "SC_HXR" "SC_SXR" "SC_DIAG0" "SC_BSYD")
 p_types=("LIVE" "DESIGN")
+
+error_message(){
+    msg="This is an automated email notification from the cron job monitoring the "
+    msg+="execution of the BLEM PV Service. The script encountered an error due to "
+    msg+="a duplicate process running with the same Beam Path & PV Type arguments.\n\n"
+
+    msg+="Error Details:\n\n"
+    msg+="  - Timestamp:\t\t`date +"%Y-%m-%d %T"`\n"
+    msg+="  - Beam Path:\t\t$1\n"
+    msg+="  - PV Type:\t\t$2\n"
+    msg+="  - Process PID:\t$3\n"
+    msg+="  - Script Name:\tpython blem_pv.py $1 $2\n\n"
+
+    msg+="Action Required:\n"
+    msg+="Please investigate and resolve the duplicate process to prevent conflicts "
+    msg+="and ensure the smooth execution of subsequent runs.\n"
+}
 
 # Start the python script for every path & type combination if it is not running or stuck on exit
 for arg1 in "${b_paths[@]}"; do
@@ -26,11 +44,14 @@ for arg1 in "${b_paths[@]}"; do
                 continue
             fi
 
-            kill $pgrep_out
+            kill -9 $pgrep_out
 
             error_pv="BLEM:SYS0:1:$arg1:$arg2:ERR_CNT"
             err_cnt=$(caget -t $error_pv)
-            caput $error_pv $(($err_cnt + 1)) > /dev/null
+            caput $error_pv $(($err_cnt + 1))
+
+            error_message $arg1 $arg2 $pgrep_out
+            printf %b "$msg" | mailx -s "Cron Job Error - Duplicate BLEM PV Process" zdomke@slac.stanford.edu
         fi
 
         python blem_pv.py $arg1 $arg2 &
