944764a682570509359e289d194ca9e6cb391336
Zach Domke <zdomke@slac.stanford.edu>
2023-08-03 15:06:43 -0700

    Update Default Environment source

--- blem_pv.sh
+++ blem_pv.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
 # Source Default Environment & Python3.7 to use Matlab 2020a w/ python's matlab.engine
-source /afs/slac/g/lcls/tools/script/ENVS64.bash
+if [ -d /afs/slac/g/lcls ]; then
+    source /afs/slac/g/lcls/tools/script/ENVS64.bash
+elif [ -d /usr/local/lcls ]; then
+    source /usr/local/lcls/tools/script/ENVS64.bash
+fi
 source $PACKAGE_TOP/anaconda/envs/python3.7env/bin/activate
 export PYTHONPATH=$TOOLS/python/toolbox
 export LD_LIBRARY_PATH=/usr/local/lcls/package/anaconda/envs/python3.7env/epics/lib/linux-x86_64:$LD_LIBRARY_PATH
